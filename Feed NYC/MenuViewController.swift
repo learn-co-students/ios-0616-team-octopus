c644987e79ffe2591b7b52d649e4b8f4b4fb8c2a
hdinhof1 <hdinhof1@binghamton.edu>
2016-08-08 14:47:14 -0400

    Add three viewControllers, containerVC is the master view controller, topViewController is the first view we see, and MenuViewController is the menu view, Sidebar

---
+++ Feed NYC/MenuViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MenuViewController.swift
+//  Feed NYC
+//
+//  Created by Henry Dinhofer on 8/8/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // When you want to instantiate a new ViewController from Menu do two things:
+    // 1) Instantiate new VC:
+    //      let profileViewController =
+    //      self.storyboard!.instantiateViewControllerWithIdentifier("profileViewController")
+    
+    // 2) Set self.so_containerViewController.topViewController equal to new VC:
+    //     self.so_containerViewController.topViewController = profileViewController
+    @IBAction func AboutUsTapped(sender: AnyObject) {
+        
+    }
+    
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Feed NYC/TopViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TopViewController.swift
+//  Feed NYC
+//
+//  Created by Henry Dinhofer on 8/8/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class TopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showMenu(sender: AnyObject) {
+        if let container = self.so_containerViewController
+        {
+            container.isSideViewControllerPresented = true
+            
+            // To close the sidebar menu set is sideVCPresented to false
+        }
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
