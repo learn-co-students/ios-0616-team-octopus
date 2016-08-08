a315777e78d351100412d35582f692c3e7b6425d
JennyShalai <jenny.shalai@gmail.com>
2016-08-08 13:50:55 -0400

    two markers tow colors and center of the map shifted a little

--- Feed NYC/ViewController.swift
+++ Feed NYC/ViewController.swift
@@ -14,17 +14,26 @@ class ViewController: UIViewController {
     
     override func loadView() {
         // map possition at start
-        let camera = GMSCameraPosition.cameraWithLatitude(40.723074, longitude: -73.986348, zoom: 11.0)
+        let camera = GMSCameraPosition.cameraWithLatitude(40.738440, longitude: -73.950498, zoom: 11.0)
         let mapView = GMSMapView.mapWithFrame(CGRect.zero, camera: camera)
         mapView.myLocationEnabled = true
         view = mapView
         
-        // Marker
+        // Marker Green
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: 40.723074, longitude: -73.986348)
         marker.title = "Test"
         marker.snippet = "test"
+        marker.icon = GMSMarker.markerImageWithColor(UIColor.greenColor())
         marker.map = mapView
+        
+        // Marker Blue
+        let marker1 = GMSMarker()
+        marker1.position = CLLocationCoordinate2D(latitude: 40.717540, longitude: -74.001620)
+        marker1.title = "Test"
+        marker1.snippet = "test"
+        marker1.icon = GMSMarker.markerImageWithColor(UIColor.blueColor())
+        marker1.map = mapView
     }
 
 
