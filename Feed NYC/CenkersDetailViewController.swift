845a5a11812d128b466e87ca56be3abd71bcc98f
github username <cenkerdemir@yahoo.com>
2016-08-09 18:46:40 -0400

    ff

--- Feed NYC/CenkersDetailViewController.swift
+++ Feed NYC/CenkersDetailViewController.swift
@@ -14,6 +14,8 @@ class CenkersDetailViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
