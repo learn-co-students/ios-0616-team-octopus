6cfc3df1f6d88ad88d99bdf1814125d1da73c535
dennisvera <dennisvera.z@gmail.com>
2016-08-09 17:56:39 -0400

    reworked xml parser to capture all keys

--- Feed NYC/FacilityDetails.swift
+++ Feed NYC/FacilityDetails.swift
@@ -14,9 +14,19 @@ class FacilityDetails: CustomStringConvertible {
     var name = String()
     var briefDescription = String()
     var streetAddress = String()
+    var city = String()
+    var state = String()
+    var zipcode = String()
     var phoneNumber = String()
+    var hoursOfOperation = String()
+    var intake = String()
+    var fee = String()
+    var featureList: [String] = []
+    var eligibility = String()
+    var requiredDocuments = String()
+    
     
     var description: String {
-        return "name: \(name), briefDescription: \(briefDescription), streetAddress: \(streetAddress), phoneNumber: \(phoneNumber)"
+        return "name: \(name)\n, briefDescription: \(briefDescription)\n, streetAddress: \(streetAddress)\n, city: \(city)\n, state: \(state)\n, zipcode: \(zipcode)\n, phoneNumber: \(phoneNumber)\n, hoursOfOperation: \(hoursOfOperation)\n, intake: \(intake)\n, fee: \(fee)\n, featureList: \(featureList)\n, eligibility: \(eligibility)\n, requiredDocuments: \(requiredDocuments)"
     }
 }
